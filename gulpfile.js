c7968ea59c98c4c8fb02e2c3c5a34c1da94299e8
Mehuge <adf@sorcerer.co.uk>
2015-07-26 11:30:32 +0100

    Very basic example UI / project structure

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+'use strict';
+
+// to debug gulp, use
+// node --debug node_modules/gulp/bin/gulp.js
+
+var gulp = require('gulp'),
+  watcher = require('gulp-watch'),
+  connect = require('gulp-connect'),
+  del = require('del'),
+  gutil = require('gulp-util'),
+  notify = require('gulp-notify'),
+  sourcemaps = require('gulp-sourcemaps'),
+  sourcemapReporter = require('jshint-sourcemap-reporter'),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+  watchify = require('watchify'),
+  browserify = require('browserify'),
+  babelify = require('babelify'),
+  reactify = require('reactify'),
+  react = require('gulp-react'),
+  livereload = require('gulp-livereload'),
+  stylus = require('gulp-stylus');
+
+function buildJS(watch) {
+  var bundler, compile;
+  bundler = browserify('src/mehuge-kills/main.js', watchify.args);
+
+  if (watch) {
+    bundler = watchify(bundler);
+  }
+
+  bundler.transform(babelify.configure({
+    optional: ['es7.asyncFunctions','es7.decorators']
+  }));
+  bundler.transform(reactify);
+
+  compile = function() {
+    return bundler.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .on('prebundle', function(bund) {
+        // Make React available externally for dev tools
+        bund.require('react');
+      })
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write(''))
+      .pipe(gulp.dest('./dist/mehuge-kills')).pipe(livereload());
+  }
+
+  bundler.on('update', compile);
+  return compile();
+}
+
+function buildStyles(watch) {
+  var compile = function() {
+    return gulp.src('src/**/**.styl')
+    .pipe(stylus({set:['compress','include css']}))
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload());
+  }
+
+  if (watch) {
+    livereload.listen();
+    watcher(['src/**/*.styl','src/**/*.css'], compile);
+  }
+  return compile();
+}
+
+function copyCSS() {
+  return gulp.src('src/**/**.css').pipe(gulp.dest('./dist'));
+}
+
+gulp.task('clean', function (cb) { del(['dist'], cb); });
+gulp.task('js', [ 'clean' ], buildJS);
+gulp.task('style', [ 'clean' ], buildStyles);
+gulp.task('css', [ 'clean'], copyCSS);
+
+gulp.task('images', ['clean'], function() {
+  return gulp.src('src/**/images/*')
+          .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', [ 'style', 'css', 'js', 'images', 'clean' ], function() {
+  return gulp.src('src/**/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+// Runs web server
+gulp.task('server', function () { connect.server({ root: 'dist', port: 9000 }); });
+
+// builds site, runs server
+gulp.task('default', [ 'build', 'server' ]);
