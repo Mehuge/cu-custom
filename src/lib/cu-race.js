41ca4a79c9d4673d2c35a57efc4ef405e081c3aa
Mehuge <adf@sorcerer.co.uk>
2015-08-03 23:51:28 +0100

    Add cu-race definitions.

---
+++ src/lib/cu-race.js
@@ -0,0 +1,24 @@
+module.exports = {
+    //Tuatha = 0,
+    1: "Hamadryad",
+    2: "Luchorpan",
+    3: "Firbog",
+
+    4: "Valkyrie",
+    5: "Helbound",
+    6: "FrostGiant",
+    //Dvergr = 7,
+
+    8: "Strm",
+    9: "CaitSith",
+    10: "Golem",
+    //Gargoyle = 11
+
+    12: "StormRider",
+    13: "StormRiderT",
+    14: "StormRiderV",
+    
+    15: "HumanMaleV",
+    16: "HumanMaleA",
+    17: "HumanMaleT"
+};
